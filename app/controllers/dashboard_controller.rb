ef891f10c7251a81807423565100aa28d9a1e6a6
brettgrigsby <gbrett@gmail.com>
2015-07-23 13:17:07 -0600

    fix link in feed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,16 @@
 class DashboardController < ApplicationController
+  helper_method :commit_link
   def show
     @client = Octokit::Client.new(access_token: current_user.token)
     @events = @client.user_events(current_user.nickname)
 #    byebug
+  end
+
+  def commit_link(event)
+    if event.payload.commits
+      "http://github.com/#{event.repo.name}/commits/#{event.payload.commits.first.sha}"
+    else
+      "http://github.com/#{event.repo.name}"
+    end
   end
 end
